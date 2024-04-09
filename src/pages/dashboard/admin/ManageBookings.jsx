ef0444f55f91f9556190c166605b8e24e0df77da
Amreshup <138227480+Amreshup@users.noreply.github.com>
2024-04-09 12:55:43 +0530

    Update ManageBookings.jsx

--- src/pages/dashboard/admin/ManageBookings.jsx
+++ src/pages/dashboard/admin/ManageBookings.jsx
@@ -16,7 +16,7 @@ const ManageBookings = () => {
     enabled: !loading,
     queryFn: async () => {
       const res = await fetch(
-        `http://localhost:5000/payments/all`,
+        `https://royalties-buffet-server.onrender.com/payments/all`,
         {
           headers: {
             authorization: `Bearer ${token}`,
